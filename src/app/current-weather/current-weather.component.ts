b46afcb4ff0c46dbb18b206d2972a90ca1bf6ccc
ramyavanisoma <ramyavani.321@gmail.com>
2023-03-12 16:18:24 -0700

    initial commit

---
+++ src/app/current-weather/current-weather.component.ts
@@ -0,0 +1,24 @@
+import { Component ,OnInit} from '@angular/core';
+import { ICurrentWeather } from '../icurrent-weather';
+
+@Component({
+  selector: 'app-current-weather',
+  templateUrl: './current-weather.component.html',
+  styleUrls: ['./current-weather.component.css']
+})
+export class CurrentWeatherComponent implements OnInit{
+  current:ICurrentWeather;
+constructor(){
+  this.current={
+    city:'Lynnwood',
+    country:'US',
+    date:new Date(),
+    image:'',
+    temperature:45,
+    description:"Mostly Cloudy"
+  }
+}
+ngOnInit(): void {
+    
+}
+}
